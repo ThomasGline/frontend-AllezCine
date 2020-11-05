28025a1eb4cd4b1ecfb9f2770ca6df494dd63f4e
Thomas Gline <glinethomas20@gmail.com>
2020-11-05 16:39:24 +0100

    script

--- script.js
+++ script.js
@@ -78,13 +78,9 @@ buttonElement.onclick = function (event) {
     fetch(newUrl)
         .then((res) => res.json())
         .then((data) => {
-<<<<<<< HEAD
             movies = data.result;
             const movieBlock = createMovieContainer(movies);
             movieSearchable.appendChild(movieBlock);
-=======
-            // data.results []
->>>>>>> thomas
             console.log('Data : ', data);
         })
         .catch((error) => {
@@ -92,18 +88,17 @@ buttonElement.onclick = function (event) {
         });
     inputElement.value = '';
     console.log('Value : ', value);
-<<<<<<< HEAD
 };
 
-function movieSection(movies){
-    return movies.map((movie) =>{
+function movieSection(movies) {
+    return movies.map((movie) => {
         return `
             <img src=${IMAGE_URL + movie.poster_path} data-movie-id=${movie.id}/>
         `;
     })
 }
 
-function createMovieContainer(movies){
+function createMovieContainer(movies) {
     const movieElement = document.createElement('div');
     movieElement.setAttribute('class', 'movie');
 
@@ -119,84 +114,3 @@ function createMovieContainer(movies){
     movieElement.innerHTML = movieTemplate;
     return movieElement;
 }
-=======
-}
-
-let image;
-let title;
-let year;
-let style;
-//const card = document.querySelector('.container .row .')
-
-let specurl = 'https://api.themoviedb.org/3/movie/popular?api_key=a2ccba0e981edfbbb30762594da0816b';
-
-document.getElementById('btnAction').addEventListener('click', function () {
-    //event.preventDefault();
-    const value = 'action';
-
-    const newUrl = url + '&query=' + value;
-
-    fetch(newUrl)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log('Data : ', data);
-        })
-        .catch((error) => {
-            console.log('Error : ', error);
-        });
-    inputElement.value = '';
-    console.log('Value : ', value);
-
-    document.getElementById('test').innerHTML += `
-    <div class="col-2">
-        <div class="card">
-            <img class="card-img-top" src="img/god.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">God's Compass</h5>
-                <p class="card-text">2016</p>
-                <p class="card-text">genre</p>
-            </div>
-        </div>
-    </div>`;
-    console.log('coucou');
-})
-
-
-function generateFirstCard() {//Fonction qui génère les 5 premières card
-    let counter = 5;
-    while (counter >= 1) {
-        document.getElementById('firstMovie').innerHTML += `
-        <div class="col-2">
-        <div class="card">
-            <img class="card-img-top" src="img/citizen.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Citizen Soldier</h5>
-                <p class="card-text">2016</p>
-                <p class="card-text">genre</p>
-            </div>
-        </div>
-    </div>`;
-        counter--;
-    }
-}
-generateFirstCard();
-
-function generateFeatureCard() {//Fonction qui génère les card de feature
-    let counter = 12;
-    while (counter >= 1) {
-        document.getElementById('featureMovies').innerHTML += `
-        <div class="col-2">
-        <div class="card">
-            <img class="card-img-top" src="img/citizen.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Citizen Soldier</h5>
-                <p class="card-text">2016</p>
-                <p class="card-text">genre</p>
-            </div>
-        </div>
-    </div>`;
-        counter--;
-    }
-}
-generateFeatureCard();
->>>>>>> thomas
