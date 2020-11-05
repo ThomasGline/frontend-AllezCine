e279769eacda75dfafcd31c4e4de3190aa95e911
Thomas Gline <glinethomas20@gmail.com>
2020-11-05 11:24:24 +0100

    search data

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ $(document).ready(function () {
 });
 
 const API_KEY = 'a2ccba0e981edfbbb30762594da0816b';
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=a2ccba0e981edfbbb30762594da0816b';
 
 //button search
 const buttonElement = document.querySelector('#search');
@@ -39,5 +40,16 @@ const inputElement = document.querySelector('#inputValue');
 buttonElement.onclick = function (event) {
     event.preventDefault();
     const value = inputElement.value;
-    console.log('Value : ' + value);
+
+    const newUrl = url + '&query=' + value;
+
+    fetch(newUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log('Data : ', data);
+        })
+        .catch((error) => {
+            console.log('Error : ', error);
+        });
+    console.log('Value : ', value);
 }
